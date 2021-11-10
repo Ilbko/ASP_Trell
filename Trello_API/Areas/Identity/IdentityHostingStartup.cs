a9851924d9f77ff6b410efc8a1a5d6f7df5ee8d2
Илья <@DESKTOP-ALQEF61>
2021-11-10 02:15:56 +0200

    Adding an API+Identity

---
+++ Trello_API/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Trello_API.Areas.Identity.Data;
+using Trello_API.Data;
+
+[assembly: HostingStartup(typeof(Trello_API.Areas.Identity.IdentityHostingStartup))]
+namespace Trello_API.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<DbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<DbContext>();
+            });
+        }
+    }
+}
