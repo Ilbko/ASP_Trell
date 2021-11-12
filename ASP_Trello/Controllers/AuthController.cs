f3468e2df85d421442df9e2dd6ed180c78d47c70
Илья <@DESKTOP-ALQEF61>
2021-11-12 03:28:58 +0200

    Layouts and views for login/signup pages

---
+++ ASP_Trello/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_Trello.Controllers
+{
+    public class AuthController : Controller
+    {
+        public AuthController()
+        {
+
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            ViewData["Title"] = "Sign Up";
+
+            return View();
+        }
+    }
+}
